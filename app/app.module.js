7cd4ecb35db3a32567f662a97cdfa67e863acd8a
Jose Manuel <josedepaz9@gmail.com>
2016-05-04 21:55:26 -0600

    DEV: style fixed

--- app/app.module.js
+++ app/app.module.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    angular.module('app', ['ngRoute'])
+    angular.module('app', ['ngRoute', 'ngSanitize'])
         .config(configuration);
 
     configuration.$inject = ['$routeProvider'];
