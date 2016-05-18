c36c06017e2116c5eb35ea48eb3a3a1365cb1b47
Jose Manuel <josedepaz9@gmail.com>
2016-05-17 22:57:30 -0600

    MOD: loader in ranking

--- app/rankings/ranking.controller.js
+++ app/rankings/ranking.controller.js
@@ -15,15 +15,18 @@
         
         
         function activate(){
+            self.loading = true;
             $http.get('/rm-server-web/rs/rallies/' + $routeParams.rallyId + '/ranking').then(getRankingDone, getRankingFail);
         }
         
         function getRankingDone(result) {
             self.ranking = result.data;
+            self.loading = false;
         }
         
         function getRankingFail(error) {
             console.log(error);
+            self.loading = false;
         }
     }
 })();
--- app/rankings/rankings.controller.js
+++ app/rankings/rankings.controller.js
@@ -15,15 +15,18 @@
         
         
         function activate(){
+            self.loading = true;
             $http.get('/rm-server-web/rs/rallies').then(getRalliesDone, getRalliesFail);
         }
         
         function getRalliesDone(result) {
             self.rallies = result.data;
+            self.loading = false;
         }
         
         function getRalliesFail(error) {
             console.log(error);
+            self.loading = false;
         }
     }
 })();
