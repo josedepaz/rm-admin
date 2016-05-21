be4487787e5cad4854313399ecfd1a2884737c71
garfenter <garfenter@gmail.com>
2016-05-20 23:33:46 -0600

    Add redirect when user is not authenticated

--- app/main.controller.js
+++ app/main.controller.js
@@ -1,10 +1,28 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return null;
+}
+
 (function(){
     'use strict';
     angular.module('app')
     .controller('MainController', MainController);
-    
+
     function MainController(){
         var self = this;
         this.title = "Rally Matemático";
+        if(!getCookie("atAdmin")){
+            window.location.href = "/rm-admin/login.html";
+        }
     }
 })();
