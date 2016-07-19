598be00eb943948aac5cbfdb3cc8604cddbd7ab0
Jose Manuel <josedepaz9@gmail.com>
2016-07-18 19:52:27 -0600

    ADD: question configuration logic

--- app/questions/questions.controller.js
+++ app/questions/questions.controller.js
@@ -9,11 +9,22 @@
     function ConfigQuestionsController($http) {
         var vm = this;
         
+        vm.questions = [];
 
         activate();
 
         ////////////////
 
-        function activate() { }
+        function activate() {
+            $http.get('/rm-server-web/rs/config/question')
+            .then(function(result){
+                vm.questions = result.data.filter(function(item){
+                    item.type === 'question';
+                });
+            })
+            .catch(function(err){
+                console.error(err);
+            });
+        }
     }
 })();
